9bd3f26ed4e8f0d211e046ea55dfecba71838b2e
Iason-PC\Iason <wenyingwei@cainfoharbor.com>
2018-08-27 17:45:40 +0800

    测试

---
+++ src/main/java/com/iason/configserver/config/DataSourceConfig.java
@@ -0,0 +1,35 @@
+package com.iason.configserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Iason on 2018/8/27.
+ */
+@Component
+@PropertySource(value = "classpath:encryption.yml")
+public class DataSourceConfig {
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	@Value("${username}")
+	private String userName;
+
+	@Value("${password}")
+	private String passWord;
+}
